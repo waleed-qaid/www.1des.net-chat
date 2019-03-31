1c75aa718ce5353c62c7d0e650c358cd7e86a42a
abo-rateel <48714157+abo-rateel@users.noreply.github.com>
2019-03-31 17:25:17 +0300

    Add files via upload

---
+++ www.1des.net-chat-te3b.com.js
@@ -0,0 +1,128 @@
+$(`<img src="https://www.te3b.com/sico/z1d7a2217710.png" class="fr" style="width:100%;margin-top: 0px;">
+
+<a id="des22" style="float:right;margin:-68px 2px 2px;transform:matrix(0, -1, 1, 0, 22, 10);color:rgb(252, 71, 71);" class="label label label-primary mini fl" target="_blank" href="https://www.te3b.com">فعاليات الشات</a>
+<a id="des22" style="margin:-68px 2px 2px;transform:matrix(0, -1, 1, 0, -13, 4);float:right;color:rgb(255, 217, 0);" class="label label label-primary mini fl" target="_blank" href="https://www.te3b.com"> الأشتــراكات</a>
+<a id="des22" style="margin:-75px 2px 2px;transform:matrix(0, -1, 1, 0, -46, -5);float:right;color: rgb(107, 255, 107);" class="label label label-primary mini fl " target="_blank" href="https://www.te3b.com">&nbsp;&nbsp;الأداره&nbsp;&nbsp;&nbsp;&nbsp;</a>
+<a id="des22" style="width:90px;margin:2px;transform:matrix(1, 0, 0, 1, 6, 0);" class="label label label-primary mini fl" target="_blank" href="https://www.te3b.com">شات كتابي </a>
+<a id="des22" style="width:90px;margin:2px;transform:matrix(1, 0, 0, 1, 2, 0);" class="label label label-primary mini fl" target="_blank" href="https://fkamh.com/">شات حيروني </a>
+<a id="des22" style="width:92px;margin:2px;transform:matrix(1, 0, 0, 1, -2, 0);" class="label label label-primary mini fl" target="_blank" href="https://www.bnatt3iz.com/">شات بنات تعز</a>
+<a id="des22" style="width:97px;margin:2px;transform:matrix(1, 0, 0, 1, 8, 0);" class="label label label-primary mini fl" target="_blank" href="https://www.1des.net/">صـــمم بنـــرك</a>
+<a href="https://chatmaladh.net/" target="_blank" id="">
+    <img src="https://www.te3b.com/sico/z1d6g25kkd10.gif" style="width: 70%;transform:matrix(1, 0, 0, 1, 8, 0);">
+	</a>
+`).insertBefore('.nav-tabs');
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('a[href="https://jawalhost.com/"]').removeAttr('href');
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="">
+$(`<style>
+.fr.borderg, .fr.borderg a {
+    font-size: 100% !important;
+    padding: 0 5px!important;
+    border-radius: 0px 0px 0px 7px!important;
+    margin-top: 0px!important;
+    margin-right: 0px!important;
+    background-color: #333333!important;
+    border: black solid 0px;
+    color: #fff;
+}
+a.label.label-primary.fl img.fl {
+    display: none;
+}
+a.label.label-primary.fl {
+    border-radius: 3px!important;
+    margin: 2px -110px;
+    padding: 5px!important;
+    margin-bottom: 2px;
+    font-size: 100%!important;
+    text-align: right!important;
+
+}
+#des22 {
+
+    text-align: center!important;
+}
+button.btn.fr.btn-success.fa.fa-refresh {
+    display: none;
+}
+label.label.label-primary.mini.fl {
+    width: 65.1%!important;
+    padding: 1px;
+}
+span.s1.fa.fa-user.label.badgex.label-as-badge.label-success {
+    padding: 0px 3px 2px;
+    margin-right: 5px;
+}
+.fr.borderg, .fr.borderg a {
+    font-size: 12px !important;
+    padding: 0 5px!important;
+}
+label.fl.label.loginstat {
+	background-color: transparent!important;
+    border-radius: 5px!important;
+    position: absolute;
+    top: 0;
+	left: 0;
+    margin: 2px -4px;
+    padding: 2px 8px 4px!important;
+    margin-top: 4px;
+    font-size: 100%!important;
+}
+ul.nav.nav-tabs.fl li .fa-user:before,.fa-user-plus:before {
+    display: none
+}
+ul.nav.nav-tabs.fl li {
+    width: 33.3%;
+    margin-left: -2px;
+    border-radius: 50px;
+	margin-bottom: 5px!important;
+	
+}
+ul.nav.nav-tabs.fl li a {
+    background-color: #333333!important;
+    color: #fff;
+    margin: auto;
+    text-align: center;
+    width: 95%;
+    border-radius: 3px;
+    margin-top: 2px;
+    padding: 2px 0!important;
+    margin-right: 67px;
+    border: 0px solid #4c4b4b
+}
+ul.nav.nav-tabs.fl {
+   background: none!important;
+}
+</style>`).insertBefore('body');
+$(`<center><div style="position: static;width: 100%;" onclick="setTimeout(function(){fixSize();},800);"><div width="99.5%" id="design-a"style="background-color: antiquewhite;color: #105873;font-family: jazeera-light, FontAwesome;border-bottom: 3px solid #105873;border-top: 3px solid #6b5f5f;padding-bottom: 2px;border-radius: 0px 0px 15px 15px;" ><font id="sultan" class=" sultan11"style="
+    font-family: jazeera-light, FontAwesome;
+" >  <u><b>الاشتراكــات</b></u> </font><div id="spanan" style="font-family: jazeera-light, FontAwesome;color: red;text-align: right;display:none;" >  
+		<p style="text-align: center"><b><font color="#000080">اشتراكات الشات</font></b>
+		</p>
+</p>
+<div style="float: right;padding-right: 20px;">- 1</div> تاج ذهبي بخاصية ( 
+		المخفي ) + ( سوبر اخضر هديه ) بـ 600 ريال لمدة : شهرين <img src="https://www.te3b.com/sico/z1d63jlqlq10.gif"> 
+</p>
+<div style="float: right;padding-right: 20px;">- 2</div> تاج اسود ملكي بخاصية ( 
+		المخفي ) بـ 500 ريال لمدة : شهرين <img src="https://www.te3b.com/sico/z1d3c130e710.GIF">
+</p>
+<div style="float: right;padding-right: 20px;">- 3</div> تاج فضي بـ 400 ريال 
+		لمدة : شهرين <img src="https://www.te3b.com/sico/z1d3c137ha10.gif"> 
+</p>
+<div style="float: right;padding-right: 20px;">- 4</div> نجمه ذهبيه بـ 300 ريال 
+		لمدة : شهرين <img src="https://www.te3b.com/s1.gif"> 
+</p>
+<div style="float: right;padding-right: 20px;">- 5</div> نجمه سوداء بـ 200 ريال 
+		لمدة : شهرين <img src="https://www.te3b.com/s2.gif"> 
+</p>
+<div style="float: right;padding-right: 20px;">- 6</div> نجمه فضيه بـ 150 ريال 
+		لمدة : شهرين <img src="https://www.te3b.com/s3.gif"> 
+		
+
+		<p>
+		<b>ومن اراد بنر لـ اسمه بـ 100 ريال مع التصميم ولمدة
+		<font color="#0000FF">شهرين</font></b><p style="text-align: center">
+		<font color="#800000"><u><b>
+		<a target="_blank" href="https://www.te3b.com/sico/z1cj3oqojb10.JPG">وللتواصل مع الاداره اضغط هنا</a></b></u></font></div> </div></div><center>`).insertBefore('#d2');
+    $("#design-a").click(function(){$("#spanan").slideToggle(function(){});});
+eval((emo+'').replace('[0-9][0-9]|','[0-9][0-9][0-9]|[0-9][0-9]|'));
